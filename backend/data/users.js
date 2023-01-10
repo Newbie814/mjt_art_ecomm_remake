fb0007ae87eba41fd4e4e26325dc7d6dcef72a79
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2023-01-10 16:47:30 -0500

    mock data seeded into mongo

--- backend/data/users.js
+++ backend/data/users.js
@@ -5,20 +5,18 @@ const users = [
     name: 'Myrtis Thomas',
     email: 'admin@example.com',
     password: bcrypt.hashSync('123456', 10),
-    password: xxxx,
+
     isAdmin: true,
   },
   {
     name: 'Steve Rogers',
     email: 'THE_Cap@example.com',
     password: bcrypt.hashSync('123456', 10),
-    password: xxxx,
   },
   {
     name: 'Ned Stark',
     email: 'headless@example.com',
     password: bcrypt.hashSync('123456', 10),
-    password: xxxx,
   },
 ];
 

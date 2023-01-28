30c61a1bd262868f5106b5ab9148b92384495c28
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2023-01-27 20:21:57 -0500

    setup store state for homescreen

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,32 +1,36 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import { useDispatch, useSelector } from 'react-redux';
 import { Row, Col } from 'react-bootstrap';
-
+import { listProducts } from '../actions/productActions';
 import Product from '../components/Product';
 
 const HomeScreen = () => {
-  const [products, setProducts] = useState([]);
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    const fetchProducts = async () => {
-      const { data } = await axios.get('/api/products');
+    dispatch(listProducts());
+  }, [dispatch]);
 
-      setProducts(data);
-    };
+  const productList = useSelector((state) => state.productList);
 
-    fetchProducts();
-  }, []);
+  const { loading, error, products } = productList;
 
   return (
     <>
       <h1>Latest Products</h1>
-      <Row>
-        {products.map((product) => (
-          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-            <Product product={product} />
-          </Col>
-        ))}
-      </Row>
+      {loading ? (
+        <>Loading...</>
+      ) : error ? (
+        <h3>{error}</h3>
+      ) : (
+        <Row>
+          {products.map((product) => (
+            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+              <Product product={product} />
+            </Col>
+          ))}
+        </Row>
+      )}
     </>
   );
 };

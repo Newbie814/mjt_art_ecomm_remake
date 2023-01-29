df1256bbec12bed6af509da00739074fcf6aeca8
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2023-01-28 19:28:43 -0500

    building out cart screen data implementation

--- frontend/src/screens/CartScreen.jsx
+++ frontend/src/screens/CartScreen.jsx
@@ -1,6 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Link, useParams, useLocation } from 'react-router-dom';
+import {
+  Row,
+  Col,
+  ListGroup,
+  Image,
+  Form,
+  Button,
+  Card,
+} from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import { addToCart } from '../actions/cartActions';
+import Message from '../components/Message';
 
 const CartScreen = () => {
+  const params = useParams();
+  const dipatch = useDispatch();
+  const location = useLocation();
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
+
+  const qty = location.search ? Number(location.search.split('=')[1]) : 1;
+
+  useEffect(() => {
+    if (params.id) {
+      dipatch(addToCart(params.id, qty));
+    }
+  }, [dipatch, params.id, qty]);
   return (
     <div>
       <h1>This is my cart</h1>

f8355c152ed697c62f7ce1374ec3fbd1ea6bcd68
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2023-02-22 17:13:30 -0500

    setup getuserprofile function to return user object minus token

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -28,22 +28,19 @@ const authUser = asyncHandler(async (req, res) => {
 // @route   GET /api/users/profile
 // @access  Private
 const getUserProfile = asyncHandler(async (req, res) => {
-  res.send('Success');
+  const user = await User.findById(req.user._id);
 
-  // const user = await User.findById({ _id: req.user._id });
-
-  // if (user && (await user.matchPassword(password))) {
-  //   res.json({
-  //     _id: user._id,
-  //     name: user.name,
-  //     email: user.email,
-  //     isAdmin: user.isAdmin,
-  //     token: generateToken(user._id),
-  //   });
-  // } else {
-  //   res.status(401);
-  //   throw new Error('Invalid email or password');
-  // }
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
 });
 
 export { authUser, getUserProfile };

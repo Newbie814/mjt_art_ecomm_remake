f35fa9b382977ba8f2fadac41a11491917fd0db7
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2023-02-24 13:40:34 -0500

    setup route to create new users, created middleware to encrypt password before save to database

--- backend/data/products.js
+++ backend/data/products.js
@@ -1,122 +1,123 @@
-const products = [
-  {
-    name: 'Unlost Marbles',
-    image:
-      'https://res.cloudinary.com/dylvkdabj/image/upload/v1646166932/website%20pics%20family/marbles-bowl_nw7rvj.jpg',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostru.',
-    brand: 'MJThomas',
-    category: 'watercolor',
-    price: 499.99,
-    countInStock: 1,
-    rating: 4.5,
-    numReviews: 12,
-  },
-  {
-    name: 'Peaches colored pencil',
-    image:
-      'https://res.cloudinary.com/dylvkdabj/image/upload/v1646166932/website%20pics%20family/peach_hski4l.jpg',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostru.',
-    brand: 'MJThomas',
-    category: 'Colored Pencil',
-    price: 599.99,
-    countInStock: 7,
-    rating: 4.0,
-    numReviews: 8,
-  },
-  {
-    name: 'Red Pepper Colored Pencil',
-    image:
-      'https://res.cloudinary.com/dylvkdabj/image/upload/v1646166933/website%20pics%20family/red-pepper_ikonrz.jpg',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostru.',
-    brand: 'MJThomas',
-    category: 'Colored Pencil',
-    price: 929.99,
-    countInStock: 5,
-    rating: 3,
-    numReviews: 12,
-  },
-  {
-    name: 'Decoupage octopus cup',
-    image:
-      'https://res.cloudinary.com/dylvkdabj/image/upload/v1646166932/website%20pics%20family/octopus-cup_dw79qo.jpg',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostru.',
-    brand: 'MJThomas',
-    category: 'Decoupage',
-    price: 399.99,
-    countInStock: 11,
-    rating: 5,
-    numReviews: 12,
-  },
-  {
-    name: 'Watercolor Cherries',
-    image:
-      'https://res.cloudinary.com/dylvkdabj/image/upload/v1646166932/website%20pics%20family/cherries-bowl_nptd5h.jpg',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostru.',
-    brand: 'MJThomas',
-    category: 'Watercolor',
-    price: 499.99,
-    countInStock: 7,
-    rating: 3.5,
-    numReviews: 10,
-  },
-  {
-    name: 'Marbleized acrylic on canvas',
-    image:
-      'https://res.cloudinary.com/dylvkdabj/image/upload/v1646166932/website%20pics%20family/marble-dkblue-gold_ezu5em.jpg',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostru.',
-    brand: 'MJThomas',
-    category: 'Abstract',
-    price: 129.99,
-    countInStock: 0,
-    rating: 4,
-    numReviews: 12,
-  },
-  {
-    name: 'MFN Fish on a Plate',
-    image:
-      'https://res.cloudinary.com/dylvkdabj/image/upload/v1646166932/website%20pics%20family/fish-plate_vfkf5v.jpg',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostru.',
-    brand: 'MJThomas',
-    category: 'Abstract',
-    price: 329.99,
-    countInStock: 2,
-    rating: 4,
-    numReviews: 12,
-  },
-  {
-    name: 'Sumpin sumpin purple flower',
-    image:
-      'https://res.cloudinary.com/dylvkdabj/image/upload/v1646166933/website%20pics%20family/purple-flower_wx5xyz.jpg',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostru.',
-    brand: 'MJThomas',
-    category: 'Abstract',
-    price: 829.99,
-    countInStock: 0,
-    rating: 4,
-    numReviews: 12,
-  },
-  // {
-  //
-  //   name: 'Marbleized acrylic on canvas',
-  //   image:
-  //     'https://res.cloudinary.com/dylvkdabj/image/upload/v1646166932/website%20pics%20family/marble-dkblue-gold_ezu5em.jpg',
-  //   description:
-  //     'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostru.',
-  //   brand: 'MJThomas',
-  //   category: 'Abstract',
-  //   price: 129.99,
-  //   countInStock: 0,
-  //   rating: 4,
-  //   numReviews: 12,
-  // },
-];
+// const products = [
+//   {
+//     name: 'Unlost Marbles',
+//     image:
+//       'https://res.cloudinary.com/dylvkdabj/image/upload/v1646166932/website%20pics%20family/marbles-bowl_nw7rvj.jpg',
+//     description:
+//       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostru.',
+//     brand: 'MJThomas',
+//     category: 'watercolor',
+//     price: 499.99,
+//     countInStock: 1,
+//     rating: 4.5,
+//     numReviews: 12,
+//   },
+//   {
+//     name: 'Peaches colored pencil',
+//     image:
+//       'https://res.cloudinary.com/dylvkdabj/image/upload/v1646166932/website%20pics%20family/peach_hski4l.jpg',
+//     description:
+//       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostru.',
+//     brand: 'MJThomas',
+//     category: 'Colored Pencil',
+//     price: 599.99,
+//     countInStock: 7,
+//     rating: 4.0,
+//     numReviews: 8,
+//   },
+//   {
+//     name: 'Red Pepper Colored Pencil',
+//     image:
+//       'https://res.cloudinary.com/dylvkdabj/image/upload/v1646166933/website%20pics%20family/red-pepper_ikonrz.jpg',
+//     description:
+//       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostru.',
+//     brand: 'MJThomas',
+//     category: 'Colored Pencil',
+//     price: 929.99,
+//     countInStock: 5,
+//     rating: 3,
+//     numReviews: 12,
+//   },
+//   {
+//     name: 'Decoupage octopus cup',
+//     image:
+//       'https://res.cloudinary.com/dylvkdabj/image/upload/v1646166932/website%20pics%20family/octopus-cup_dw79qo.jpg',
+//     description:
+//       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostru.',
+//     brand: 'MJThomas',
+//     category: 'Decoupage',
+//     price: 399.99,
+//     countInStock: 11,
+//     rating: 5,
+//     numReviews: 12,
+//   },
+//   {
+//     name: 'Watercolor Cherries',
+//     image:
+//       'https://res.cloudinary.com/dylvkdabj/image/upload/v1646166932/website%20pics%20family/cherries-bowl_nptd5h.jpg',
+//     description:
+//       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostru.',
+//     brand: 'MJThomas',
+//     category: 'Watercolor',
+//     price: 499.99,
+//     countInStock: 7,
+//     rating: 3.5,
+//     numReviews: 10,
+//   },
+//   {
+//     name: 'Marbleized acrylic on canvas',
+//     image:
+//       'https://res.cloudinary.com/dylvkdabj/image/upload/v1646166932/website%20pics%20family/marble-dkblue-gold_ezu5em.jpg',
+//     description:
+//       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostru.',
+//     brand: 'MJThomas',
+//     category: 'Abstract',
+//     price: 129.99,
+//     countInStock: 0,
+//     rating: 4,
+//     numReviews: 12,
+//   },
+//   {
+//     name: 'MFN Fish on a Plate',
+//     image:
+//       'https://res.cloudinary.com/dylvkdabj/image/upload/v1646166932/website%20pics%20family/fish-plate_vfkf5v.jpg',
+//     description:
+//       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostru.',
+//     brand: 'MJThomas',
+//     category: 'Abstract',
+//     price: 329.99,
+//     countInStock: 2,
+//     rating: 4,
+//     numReviews: 12,
+//   },
+//   {
+//     name: 'Sumpin sumpin purple flower',
+//     image:
+//       'https://res.cloudinary.com/dylvkdabj/image/upload/v1646166933/website%20pics%20family/purple-flower_wx5xyz.jpg',
+//     description:
+//       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostru.',
+//     brand: 'MJThomas',
+//     category: 'Abstract',
+//     price: 829.99,
+//     countInStock: 0,
+//     rating: 4,
+//     numReviews: 12,
+//   },
+
+// {
+//
+//   name: 'Marbleized acrylic on canvas',
+//   image:
+//     'https://res.cloudinary.com/dylvkdabj/image/upload/v1646166932/website%20pics%20family/marble-dkblue-gold_ezu5em.jpg',
+//   description:
+//     'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostru.',
+//   brand: 'MJThomas',
+//   category: 'Abstract',
+//   price: 129.99,
+//   countInStock: 0,
+//   rating: 4,
+//   numReviews: 12,
+// },
+// ];
 
 export default products;
